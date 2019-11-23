76c9b5f2afece6947ab302cf6d53cc5bfba261b0
purushottam8208758977 <puruissimple@gmail.com>
2019-11-23 10:44:50 +0530

    new day ... !

--- src/services/services.js
+++ src/services/services.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const url = "http://3.18.105.180:4000"
+const url = "http://13.59.25.246:4000"
 
 export function registration(registrationData) {
     console.log("\n\n\tIn services for registration --->", registrationData)
